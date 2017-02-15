a6ec9838ce97d7b05d9fbfc6e0871dcd127a282e
John Hu <im@john.hu>
2017-02-15 16:52:42 +0800

    try to read report file name from env

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const Testsuites = require('./src/Testsuites');
 const xml = require('xml');
 const fs = require('fs');
+const path = require('path');
 const out = process.env.TEST_REPORT_PATH || process.cwd();
+const filename = process.env.TEST_REPORT_FILENAME || 'test-report.xml';
 
 module.exports = (results) => {
   const testSuites = new Testsuites(results);
   const data = xml(testSuites, { declaration: true, indent: '  ' });
-  fs.writeFileSync(`${out}/test-report.xml`, data);
+  fs.writeFileSync(path.join(out, filename), data);
   return results;
 };
